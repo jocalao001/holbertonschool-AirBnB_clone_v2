2a98f1965146983696f4f2398a3c2aa76f71d882
jocalao001 <113933963+jocalao001@users.noreply.github.com>
2023-11-18 19:57:44 -0500

    fix PEP8 validation

--- models/state.py
+++ models/state.py
@@ -13,7 +13,12 @@ if os.getenv("HBNB_TYPE_STORAGE") == "db":
 
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+        cities = relationship(
+                                "City",
+                                backref="state",
+                                cascade="all,                 
+                                delete-orphan"
+                                )
 
 else:
 
