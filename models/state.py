9f1fa9d110e079bd738995db296a6d46027a1884
jocalao001 <113933963+jocalao001@users.noreply.github.com>
2023-11-18 19:59:47 -0500

    fix identation

--- models/state.py
+++ models/state.py
@@ -16,8 +16,7 @@ if os.getenv("HBNB_TYPE_STORAGE") == "db":
         cities = relationship(
                                 "City",
                                 backref="state",
-                                cascade="all,                 
-                                delete-orphan"
+                                cascade="all, delete-orphan"
                                 )
 
 else:
